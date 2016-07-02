5edc032b3af14874c3de66d89211d34f278c29e2
jinxiaochuan <1204160989@qq.com>
2016-07-02 16:20:55 +0800

    获取请求参数

--- 5.express/6.template.js
+++ 5.express/6.template.js
@@ -18,20 +18,20 @@ app.set('views',path.join(__dirname,'views'));
  * 1. 读取模板内容 views+ 模板的相对路径
  * 2. 渲染 模板+数据
  */
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
     res.render = function(tmpl,data,callback){
-        //得到模板存放路径
+        //得到模板存放的绝对路径
         var tmplName = path.join(app.get('views'),tmpl);
        //如果说路径没有.ejs后缀的话则添加.ejs后缀
         tmplName = tmplName+(tmplName.endsWith('.ejs')?'':'.ejs');
         //读取模板文件的内容
         fs.readFile(tmplName,'utf8',function(err,content){
-            content = content.replace(/<%(\w+)%>/g,function(all,attr){//attr 表示第一个分组
+            content = content.replace(/<%=(\w+)%>/g,function(all,attr){//attr 表示第一个分组
                 //用对象的属性把对应的字符串替换掉
                 return data[attr];
             });
-            if(callback){
-                callback(null,content);
+            if(callback){//
+                callback(err,content);
             }else{
                 res.send(content);
             }
@@ -39,7 +39,7 @@ app.use(function(req,res,next){
 
     }
     next();
-});
+});*/
 app.get('/',function(req,res){
     //渲染模板 1. 模板路径 2  模板数据对象
     res.render('user',{username:'zfpx',age:8},function(err,data){
@@ -48,5 +48,20 @@ app.get('/',function(req,res){
         data = data.replace('zfpx','ZFPX');
        res.end(data);
     });
+});
+//如果你需要给所有的模板都赋一个变量的话，
+app.use(function(req,res,next){
+    res.locals.username = 'zfpx222';
+    next();
+});
+
+/**
+ * 其实真正渲染模板的数据对象是 res.locals
+ */
+app.get('/home',function(req,res){
+    res.render('home',{msg:'home'});
+});
+app.get('/contact',function(req,res){
+    res.render('contact',{msg:'contact'});
 });
 app.listen(9090);
