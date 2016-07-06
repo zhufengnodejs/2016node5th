08d034e70f29785026f03958093d2732eb145366
zhufengnodejs <zhufengnodejs@126.com>
2016-07-06 12:47:45 +0800

    监控文件变化

--- 5.gulp/3.watch.js
+++ 5.gulp/3.watch.js
@@ -14,3 +14,13 @@ gulp.task('default',function(){
       ['concat','uglify']
   );
 });
+gulp.task('watch',function(){
+  gulp.watch(
+      './src/js/*.js',
+      function(event){//当监控的文件发生变化之后调用的回调函数，并接收事件对象
+        var type = event.type;//变化的类型
+        var path = event.path;//变化的路径
+        console.log(type,path);
+      }
+  );
+});
