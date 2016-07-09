1a6121ac1527eaff99f7cf49f0bbaf919c944c3b
zhufengnodejs <zhufengnodejs@126.com>
2016-07-09 11:32:47 +0800

    合并文件

--- 12.mongoose/1.mongoose.js
+++ 12.mongoose/1.mongoose.js
@@ -5,9 +5,10 @@ mongoose.connect('mongodb://localhost/201605th');
 // 定义的集合的字段数量 字段名称和字段类型
 //定义了之后就只能保存这三个字段了，如果多传入字段会进行忽略
 var PersonSchema = new mongoose.Schema({
-    name: {type: String},
-    age: {type: Number},
-    birth: {type: Date}
+    name: {type: String,required:true},  //必须提供
+    age: {type: Number,min:0,max:180},  //最大值和最小值
+    birth: {type: Date,default:Date.now()},//指定默认值
+    home:{type:String,enum:['北京','广州']}//指定枚举值
 });
 
 
@@ -21,17 +22,37 @@ var PersonSchema = new mongoose.Schema({
 PersonSchema.methods.findSameAge = function(cb){
     return this.model('Person').find({age:this.age},cb);
 }
+//为MODE定义类方法，通过model来调用
+PersonSchema.statics.findByName = function(name,cb){
+    //this == model
+    this.find({name:new RegExp(name)},cb);
+}
+//中间件，在save方法保存之前执行的函数
+//next表示继续向下执行
+PersonSchema.pre('save',function(next){
+    // this 指的就是entity
+   this.age = this.age * 2;
+   next();
+});
 var PersonModel = mongoose.model('Person', PersonSchema);
 //传一个参数表示获取一个模型
 var PersonModel2 = mongoose.model('Person');
 //为实例创建新的方法
+PersonModel.findByName('zfpx',function(err,docs){
+    if(err){
+        console.error(err);
+    }else{
+        console.log('findByName ',docs.length);
+    }
+});
 
 console.log(PersonModel === PersonModel2);
 //根据model可以创建一个实体
 var personEntity = new PersonModel({
-    name: 'zfpx',
-    age: 8,
-    birth: new Date(Date.now() - 8 * 365 * 24 * 60 * 60*1000)
+    name:'zfpx',
+    age: 12,
+    home:'北京'
+    //birth: new Date(Date.now() - 8 * 365 * 24 * 60 * 60*1000)
 });
 //调用我们为实例上定义的新方法
 personEntity.findSameAge(function(err,docs){
@@ -41,14 +62,19 @@ personEntity.findSameAge(function(err,docs){
         console.log(docs.length);
 })
 //把当前的对象保存到数据中
-/*personEntity.save(function (err, result) {
+personEntity.save(function (err, result) {
+    // result就是保存之后的实例对象，它也是一个Entity
     if (err) {
         console.error(err);
     } else {
         console.log(result);
-
-
+        result.findSameAge(function(err,docs){
+            if(err){
+                console.error(err);
+            }else
+                console.log(docs.length);
+        })
     }
-});*/
+});
 
 
