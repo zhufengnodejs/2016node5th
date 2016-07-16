e02b52b891e66b868e8c30e7d6b73ac7cabf2748
zhufengnodejs <zhufengnodejs@126.com>
2016-07-16 10:22:22 +0800

    ajax

---
+++ 13.websocket/1.query.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.get('/query',function(req,res){
+    //发送文件的话，它的参数是一个绝对路径
+    res.sendFile(path.resolve('./1.query.html'));
+});
+//当客户端访问根目录的时候返回当前时间
+app.get('/clock',function(req,res){
+    setTimeout(function(){
+        res.send(new Date());// end方法
+    },1000);
+});
+app.listen(9090);
