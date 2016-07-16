4eac4bd456155ac0bed4a1932050a5acf53644a2
zhufengnodejs <zhufengnodejs@126.com>
2016-07-16 12:12:51 +0800

    聊天功能

--- 14.socket.io/1.server.js
+++ 14.socket.io/1.server.js
@@ -18,9 +18,8 @@ var io = require('socket.io')(server);
 io.on('connection',function(socket){
     console.log('客户端已经连接');
     socket.on('message',function(msg){
-        console.log(msg);
-        //socket.send('客户端');//['message','客户端']
-        socket.emit('message','客户端');
+        //把此消息通知给所有的人
+       io.emit('message',msg);
     });
 });
 
